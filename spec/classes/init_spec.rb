7221a0844f839ee831dd90f041c654e4b7fb57a6
Gonzalo Matheu <gmatheu@ascentio.com.ar>
2019-01-08 12:53:01 -0300

    Replacing tabs indentation with spaces

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -26,18 +26,18 @@ describe 'netdata' do
         } end
 
         it { should compile.with_all_deps }
-	it { is_expected.to contain_class('netdata::params') }
-	it { is_expected.to contain_class('netdata::install') }
-	it { is_expected.to contain_class('netdata::config') }
-	it { is_expected.to contain_class('netdata::plugin') }
-	it { is_expected.to contain_class('netdata::service') }
-	it { is_expected.to contain_exec('install') }
-	it { is_expected.to contain_service('netdata') }
+        it { is_expected.to contain_class('netdata::params') }
+        it { is_expected.to contain_class('netdata::install') }
+        it { is_expected.to contain_class('netdata::config') }
+        it { is_expected.to contain_class('netdata::plugin') }
+        it { is_expected.to contain_class('netdata::service') }
+        it { is_expected.to contain_exec('install') }
+        it { is_expected.to contain_service('netdata') }
         it { is_expected.to contain_file("#{service_file}").with('ensure' => 'present') }
         it { verify_concat_fragment_exact_contents(catalogue, 'stream.conf+01_includes', ['[stream]','  enabled = no',]) }
-	it { is_expected.to contain_file('/opt/netdata/etc/netdata/netdata.conf').with_content(/hostname = netdata.example.com/) }
-	it { is_expected.to contain_concat('/opt/netdata/etc/netdata/stream.conf') }
-	it { verify_concat_fragment_contents(catalogue, 'web_log.conf+01', /THIS FILE IS MANAGED BY PUPPET/) }
+        it { is_expected.to contain_file('/opt/netdata/etc/netdata/netdata.conf').with_content(/hostname = netdata.example.com/) }
+        it { is_expected.to contain_concat('/opt/netdata/etc/netdata/stream.conf') }
+        it { verify_concat_fragment_contents(catalogue, 'web_log.conf+01', /THIS FILE IS MANAGED BY PUPPET/) }
 
       end
     end
